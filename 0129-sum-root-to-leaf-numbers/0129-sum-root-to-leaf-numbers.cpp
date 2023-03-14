8d6421e3c3d2d81a30b16ff083b0597761867e52
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-03-14 12:50:03 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (87.90%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    /*•	Approach:
+o	As question say’s we have to traverse upto the leaf node in right and left
+o	If we observe the second example ,we have to traverse upto the leaf node and after going to the next path we  have to remember the sum we made upto to that node 
+	For example, 495, and 491 if we carefully observe
+ 
+o	We are going to use the DEPTH-FIRST SEARCH approach
+o	We’ll traverse to the end
+	While traversing we change the node’s value
+ */
+    int sumNumbers(TreeNode* root) {
+        return dfs(root,0);
+    }
+    int dfs(TreeNode* root,int cur){
+        if(!root)return 0;//if it's null then return 0;
+        cur=cur*10+root->val;//for future reference create curretn variable;
+        if(!root->left and !root->right) return cur;//if root left or root->right 
+        return dfs(root->left,cur)+dfs(root->right,cur);
+    }
+};
