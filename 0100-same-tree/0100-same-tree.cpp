e7c2d4bc51e7fd50405465e82b6b22040b231cd4
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-03-15 10:20:44 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (51.65%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -9,6 +9,20 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*Instructing the computer
+Instructions : Rescursions
+    Pseudo code :
+        create a implict stack with function object as a parameter
+            In that function call
+                do this :
+                    Every element in the stack do this :
+                          1.CHECK IF ONE OF THE TWO GIVEN NODES ARE EQUAL IF (LEAF NODE) ~RETURN THERE EQUALITY
+                          2.IF VALUES OF GIVEN TRESS ON SAME SIDE ARE EQUAL THEN CREATE TWO FUNCTION CALL ELMENTSS THAT DO CHECK LEFT OF BOTH TREES AND RIGHT OF BOTH TREE
+                          IF THESE THREE RETURN TRUE ->THEN BOTH ARE SAME TREE
+                          ELSE BOTH ARE SAME FALSE
+                    
+        
+*/
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
