5f11f1fca7b9f53eed802b3e6f45d33a30c27544
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-03-16 17:43:26 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (32.82%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -13,21 +13,33 @@ public:
     we will use the dp 
     int dp[101][101]
     */
-    int dp[101][101]{};
     int uniquePaths(int m, int n,int i=0,int j=0) 
     {
-	/*if Out of boundaries return false*/
-	if(i>=m or j>=n)return 0;
-
-	/*if valid path return true ,which send integer 1,
-	so on a particular fn call in the emplict stack*/
-	if(i==m-1 or j==n-1)return 1;
         
-    /*if already this path computed then return that value*/
-    if(dp[i][j]) return dp[i][j];   
-	//our answer is computation of this expression
-	       /*  down   */ /*	 right	*/ 
-	return dp[i][j]=uniquePaths(m,n,i+1,j)+uniquePaths(m,n,i,j+1);
+	// /*if Out of boundaries return false*/
+	//if(i>=m or j>=n)return 0;
 
+	// /*if valid path return true ,which send integer 1,
+	//so on a particular fn call in the emplict stack*/
+	//if(i==m-1 or j==n-1)return 1;
+        
+   // /*if already this path computed then return that value*/
+    //if(dp[i][j]) return dp[i][j]   
+	//our answer is computation of this expression
+	      //                      down    	            right	
+	//return dp[i][j]=uniquePaths(m,n,i+1,j)+uniquePaths(m,n,i,j+1);
+    
+        
+        //for tabulation we declare a vector 
+        //becuase we iterate each path once we need to calculate the ways to reach that cell
+        //if there is a cell means we include it to the count 
+        vector<vector<int>> dp(m,vector<int>(n,1));
+        for(auto i=1;i<m;i++)
+        {
+            for(auto j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
