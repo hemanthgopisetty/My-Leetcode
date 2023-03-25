f0605117fb1acefdb86f88bc7be7f743db2d193e
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-03-25 11:06:34 +0530

    Time: 29 ms (5.35%), Space: 5.9 MB (94.10%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
+    vector<int> dp;
+    Solution()
+    {
+        vector<int>a(31,-1);
+        dp=a;
+    }
     int fib(int n) {
-        
+        /*****************************************
+        //Bottom up ->base case to desried case *
+        // if(n<=1)return n;                    *    
+        // vector<int> dp(n+1,0);               *
+        // dp[0]=0;                             *
+        // dp[1]=1;                             *
+        // cout<<dp.size();                     *
+        // for(auto i=2;i<=n;i++)               *
+        // {                                    *
+        //     dp[i]=dp[i-1]+dp[i-2];           *
+        //     cout<<dp[i]<<"-"<<i<<endl;       *
+        // }                                    *
+        // return dp[n];                        *
+        /****************************************/
         if(n<=1)return n;
-        vector<int> dp(n+1,0);
-        dp[0]=0;
-        dp[1]=1;
-        cout<<dp.size();
-        for(auto i=2;i<=n;i++)
+        if(dp[n]!=-1)
         {
-            dp[i]=dp[i-1]+dp[i-2];
-            cout<<dp[i]<<"-"<<i<<endl;
+            return dp[n];
         }
-        return dp[n];
+        return fib(n-1)+fib(n-2);
     }
 };
