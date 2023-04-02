6da664b9a903594de6d666db82ad279d1fa93eb6
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-04-03 01:15:07 +0530

    Time: 0 ms (100.00%), Space: 12.9 MB (70.43%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root,string s,vector<string>&a)
+    {
+        //visit,left,right;
+        s+=to_string(root->val);
+        //root condition
+        if(root->left==nullptr and root->right == nullptr)
+        {
+            a.push_back(s);
+            return ;
+        }
+        //if left subtree there then traverse
+        if(root->left)
+        {
+            //in left go deep left
+            dfs(root->left,s+"->",a);
+        }
+        //if left there , thren traverse right too 
+        if(root->right)
+        {
+            //in left traversal go right after left subtree over
+            //if left subtree over go to right subtree
+            //in right subtree
+            //traverse left and go right
+            dfs(root->right,s+"->",a);
+        }
+        return ;
+    }
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string>a;
+        string temp="";
+        dfs(root,temp,a);
+        return a;
+    }
+};
