24bd02b782b174be16ddf7114b11926f4f89557f
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-04-14 18:25:02 +0530

    Time: 15 ms (79.89%), Space: 13.6 MB (15.03%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
+    //nums.lenght==200 max of 200 elements 
+    //definetly bute force will work 
+    //what about if size is >10^4
     int countKDifference(vector<int>& nums, int k) {
         int ans=0;
+        map<int,int>m;
         for(auto i=0;i<nums.size();i++)
         {
-            for(auto j=i+1;j<nums.size();j++)
-            {
-                if(abs(nums[i] - nums[j]) == k)
-                {
-                    ans++;
-                }
+            if(m.count(nums[i]-k)){
+                ans+=m[nums[i]-k];
             }
+            
+            if(m.count(nums[i]+k)){
+                ans+=m[nums[i]+k];
+            }
+            
+            m[nums[i]]++;
         }
         return ans;
     }
