06f4532cdf95976af9954f2d598232508a8a4bd0
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-04-22 14:42:39 +0530

    Time: 51 ms (13.50%), Space: 71.5 MB (13.61%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* ans = new ListNode();
+        ListNode* temp=ans;
+        
+        int carry=0;
+        while(l1!=nullptr or l2!=nullptr or carry)
+        {
+            int sum=0;
+            
+            if(l1)
+            {
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2)
+            {
+                sum+=l2->val;
+                l2=l2->next;
+            }
+            sum+=carry;
+            
+            ListNode* newnode = new ListNode(sum%10);
+            carry=sum/10;
+            temp->next=newnode;
+            temp=temp->next;
+        }
+        
+        //y ? becuase head is stored in the next pointer of ans 
+        //usually we need to write like if res=null then add the first node
+        //maintain the two prevnode and temp pointing to current
+        //this simplifies the process
+        return ans->next;
+        
+    }
+};
