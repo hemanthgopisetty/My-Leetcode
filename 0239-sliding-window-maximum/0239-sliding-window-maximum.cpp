3d58e680ca9a6ee8f9623ea474dafd99a96fc9b7
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-04-23 21:44:16 +0530

    Time: 390 ms (27.45%), Space: 148.9 MB (27.59%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -6,15 +6,19 @@ public:
         priority_queue<pair<int,int>> pq;
      while(r<nums.size())
        {
+         //push every element to the priority_queue
          pq.push({nums[r],r});
-         if(r-l+1<k)
-         {
-          r++;
-         }
+         //if cur_elem not equal to the size of k then increase r 
+         if(r-l+1<k) r++;
+         //if equal to k
          else if(r-l+1==k)
          {
+            //pop the all elements in the queue until cur<l
             while(pq.top().second<l)pq.pop();
+            //we will be having the one element every time we push 
+            //to the pq
             a.push_back(pq.top().first);
+           //increase window size on both the sides
             l++,r++;
          }
        }
