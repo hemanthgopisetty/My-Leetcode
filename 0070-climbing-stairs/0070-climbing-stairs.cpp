9ab8cee883729ccfa01db4936292dbb80360e86c
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-04-28 10:28:28 +0530

    Time: 4 ms (11.11%), Space: 6.3 MB (23.16%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    //total no of ways we can climb stairs
+    //check all the possibilites
+    int f(int n,vector<int>&dp)
+    {
+        if(n==1)return 1;
+        if(n==2)return 2;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=f(n-1,dp)+f(n-2,dp);
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+         return f(n,dp);
+    }
+};
