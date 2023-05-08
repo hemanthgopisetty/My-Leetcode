743a6e109127d3c50f6072101d3004d01f878647
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-05-08 17:42:48 +0530

    Time: 8 ms (75.58%), Space: 10 MB (50.19%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -22,7 +22,28 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>>memo(m,vector<int>(n,-1));
-        return f(memo,grid,m-1,n-1);
+                    //Memoization
+        //vector<vector<int>>memo(m,vector<int>(n,-1));
+        //return f(memo,grid,m-1,n-1);
+        
+                   //Tabulation
+        vector<vector<int>>tab(m,vector<int>(n,grid[0][0]));
+        //fill up the base cases bro
+        for(auto i=1;i<m;i++)
+        {
+           tab[i][0]=tab[i-1][0]+grid[i][0];
+        }
+        for(auto i=1;i<n;i++)
+        {
+           tab[0][i]=tab[0][i-1]+grid[0][i];
+        }
+        for(auto i=1;i<m;i++)
+        {
+            for(auto j=1;j<n;j++)
+            {
+                tab[i][j]=grid[i][j]+min(tab[i-1][j],tab[i][j-1]);
+            }
+        }
+        return tab[m-1][n-1];
     }
 };
