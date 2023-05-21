1ddd4fd07793b9d10cebb9f5787cf7b9823011ba
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-05-21 17:06:53 +0530

    Time: 435 ms (42.23%), Space: 32.2 MB (31.44%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,29 +1,25 @@
 class Solution:
-    def change(self, amount: int, coins: List[int]) -> int:
-        dp=[[-1 for __ in range(amount+1)]for _ in range(len(coins)+1)]
-        def dfs(coins,amt,i):
-            
-            if amt==0 :
-                return 1
-            
-            if i>=len(coins):
-                return 0
-            
-            
-            if dp[i][amt] != -1 :
-                return dp[i][amt]
-            
-            take=0
-            
-            if coins[i]<=amt :
-                 take=dfs(coins,amt-coins[i],i)
-            
-            notTake=dfs(coins,amt,i+1)
-            
-            dp[i][amt]=take+notTake
-            
-            return dp[i][amt]
+    def bottomUp(self, coins, target):
+        n=len(coins)
+        dp=[[0 for _ in range(target+1)]for _ in range(n)]
         
-        return dfs(coins,amount,0)
-            
+        for i in range(n):
+            dp[i][0]=1
+        for i in range(1,target+1):
+            if i%coins[0]==0:
+                dp[0][i]=1
+        for i in range(1, n): # DP as described above
+            coin = coins[i]
+            for j in range(1, target + 1):
+                if j - coin >= 0:
+                    dp[i][j] = dp[i][j - coin] + dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+        
+        return dp[-1][-1]
+                
+        
+    def change(self, amount: int, coins: List[int]) -> int:
+        return self.bottomUp(coins, amount)
+        
         
