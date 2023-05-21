ad649ba5e49d7dde92d4171a5e93e437addc311d
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-05-21 19:36:28 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (29.04%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,15 @@
-//1.RECURSIVE WAY
-//2.ITERATIVE WAY
-//Many Sub Problems 
-/*
-2^n solution giving tle
-so use memo
-*/
 class Solution {
 public:
-    
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size()+1,-1);
-        return rob(nums,dp,nums.size()-1);
-    }
-    int rob(vector<int>&nums,vector<int>&dp,int i)
-    {
-        if(i<0)return 0;
-        if(dp[i]!=-1)return dp[i];
-        return dp[i]=max(nums[i]+rob(nums,dp,i-2),rob(nums,dp,i-1));
+        int n=nums.size();
+        if(n==1)return nums[0];
+        vector<int>dp(n);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(auto i=2;i<n;i++)
+        {
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[n-1];
     }
 };
