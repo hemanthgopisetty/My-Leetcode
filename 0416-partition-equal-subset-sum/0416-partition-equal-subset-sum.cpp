8f23205b30aa8812c22956fc41257e1641033f8c
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-05-22 15:04:32 +0530

    Time: 816 ms (6.54%), Space: 13.2 MB (68.05%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,24 +1,6 @@
 class Solution {
 public:
     //"DP problems are easy to understand only if you can deduce recursive relationship in the problem"
-    bool dfs(vector<int>& nums,vector<vector<bool>>& dp,vector<vector<bool>>& visited,int idx,int sum)
-    {
-        if(sum==0)return dp[idx][sum]=true;
-        
-        if(idx>=size(nums) or sum<0)return false;
-        
-        if(visited[idx][sum])return dp[idx][sum];
-        
-        visited[idx][sum]=true;
-        
-        return dp[idx][sum]=(dfs(nums,dp,visited,idx+1,sum) 
-                                
-                                    or
-                             
-                             dfs(nums,dp,visited,idx+1,sum-nums[idx])
-                             
-                            );
-    }
     bool canPartition(vector<int>& nums) {
        
        int totalSum=accumulate(nums.begin(),nums.end(),0);
@@ -29,8 +11,35 @@ public:
         
         totalSum/=2;
         
-        vector<vector<bool>>dp(n+1,vector<bool>((totalSum)+1,false));
-        vector<vector<bool>>visited(n+1,vector<bool>((totalSum)+1,false));
-        return dfs(nums,dp,visited,0,totalSum);
+        vector<vector<bool>>dp(n+1,vector<bool>(totalSum+1,false));
+        
+        //base cases
+        dp[0][0]=true;
+        
+        for(auto i=1;i<n+1;i++)
+        {
+            dp[i][0]=true;
+        }
+        
+        for(auto j=1;j<totalSum+1;j++)
+        {
+            dp[0][j]=false;
+        }
+        
+        for(auto i=1;i<n+1;i++)
+        {
+             for(auto j=1;j<totalSum+1;j++)
+            {
+                 
+                //don't pick
+                dp[i][j]=dp[i-1][j];
+                //pick
+                if(j>=nums[i-1])
+                {
+                    dp[i][j]=(dp[i-1][j] or dp[i-1][j-nums[i-1]]);
+                } 
+            }
+        }
+        return dp[n][totalSum];
     }
 };
