9126a8839cb119fdb7dbcf47bb356be15320f2e1
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-05-22 13:36:04 +0530

    Time: 235 ms (73.55%), Space: 16.8 MB (58.37%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    bool dfs(vector<int>& nums,vector<vector<bool>>& dp,vector<vector<bool>>& visited,int idx,int sum)
+    {
+        if(sum==0)return dp[idx][sum]=true;
+        
+        if(idx>=size(nums) or sum<0)return false;
+        
+        if(visited[idx][sum])return dp[idx][sum];
+        
+        visited[idx][sum]=true;
+        
+        return dp[idx][sum]=(dfs(nums,dp,visited,idx+1,sum) 
+                                
+                                    or
+                             
+                             dfs(nums,dp,visited,idx+1,sum-nums[idx])
+                             
+                            );
+    }
+    bool canPartition(vector<int>& nums) {
+       
+       int totalSum=accumulate(nums.begin(),nums.end(),0);
+        
+        if(totalSum % 2!=0)return false;
+        
+        int n=nums.size();
+        
+        totalSum/=2;
+        
+        vector<vector<bool>>dp(n+1,vector<bool>((totalSum)+1,false));
+        vector<vector<bool>>visited(n+1,vector<bool>((totalSum)+1,false));
+        return dfs(nums,dp,visited,0,totalSum);
+    }
+};
