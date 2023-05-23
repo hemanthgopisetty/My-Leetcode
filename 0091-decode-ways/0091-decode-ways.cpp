fad4c5f7a0e0b15effd43f8c73f63f6a67f54261
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-05-23 22:26:17 +0530

    Time: 2 ms (56.48%), Space: 6.1 MB (76.89%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,31 +1,43 @@
 class Solution {
 public:
     
-    int dp[102];
-    int decode(string& s, int idx, int n)
-    {
-        if(idx < n && s[idx] == '0') return 0;
-        if(idx >= n)
-            return 1;
+//     int decode(string& s, int idx, int n)
+//     {
+//         if(idx < n && s[idx] == '0') return 0;
+//         if(idx >= n)
+//             return 1;
         
-        if(dp[idx] != -1) return dp[idx];
+//         if(dp[idx] != -1) return dp[idx];
         
-        int ways = 0;
+//         int ways = 0;
         
-        // Pick single
-        if(s[idx] != '0') ways = decode(s, idx+1, n);
+//         // Pick single
+//         if(s[idx] != '0') ways = decode(s, idx+1, n);
             
-        // Pick couple
-        if(idx+1 < n && ((s[idx] == '1' && s[idx+1] <= '9') || (s[idx]=='2' && s[idx+1] < '7')))
-           ways += decode(s, idx+2, n);
+//         // Pick couple
+//         if(idx+1 < n && ((s[idx] == '1' && s[idx+1] <= '9') || (s[idx]=='2' && s[idx+1] < '7')))
+//            ways += decode(s, idx+2, n);
            
-        return dp[idx] = ways;
-    }
+//         return dp[idx] = ways;
+//     }
     
     int numDecodings(string s) {
        
         int n = s.size();
-        memset(dp, -1, sizeof(dp));
-        return decode(s, 0, n);
+        // memset(dp, -1, sizeof(dp));
+        //return decode(s, 0, n);
+        vector<int> dp(s.size()+1);
+        dp[0]=1;
+        if(s[0]=='0')dp[1]=0;
+        else dp[1]=1;
+        for(int i=2;i<=s.size();i++){
+            int way1,way2;
+            if(s[i-1]=='0')way1=0;
+            else way1=dp[i-1];
+            if(stoi(s.substr(i-2,2))<=26&&stoi(s.substr(i-2,2))>0&&s[i-2]!='0')way2=dp[i-2];
+            else way2=0;
+            dp[i]=way1+way2;
+        }
+        return dp[s.size()];
     }
 };
