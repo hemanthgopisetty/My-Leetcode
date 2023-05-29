e5e13d1841dac198357d18c7541fa93bcfa1b6c7
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-05-29 16:29:18 +0530

    Time: 9 ms (83.55%), Space: 9.3 MB (19.66%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    //Easy Dynamic Programming => dfs+memo
+    int dfs(string& s1,string& s2,int m,int n,vector<vector<int>>&dp)
+    {
+        if(m==0)return dp[m][n]=n;
+        if(n==0)return dp[m][n]=m;
+        
+        if(dp[m][n]!=-1)return dp[m][n];
+            
+        if(s1[m-1]==s2[n-1])
+        {
+           return dp[m][n]=dfs(s1,s2,m-1,n-1,dp);
+        }
+        
+            //we have 3 opt
+            int ins =   dfs(s1,s2,m,n-1,dp);
+            int del =   dfs(s1,s2,m-1,n,dp);
+            int rep =   dfs(s1,s2,m-1,n-1,dp);
+            
+            return dp[m][n]=1+min({ins,del,rep});
+      
+    }
+    int minDistance(string word1, string word2) {
+        int n=word2.size();
+        int m=word1.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        /*
+        
+        */
+        return dfs(word1,word2,m,n,dp);
+    }
+};
