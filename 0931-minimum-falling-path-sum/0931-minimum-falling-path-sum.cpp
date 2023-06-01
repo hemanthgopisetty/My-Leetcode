af37c542a31a468b920aad4bd00fa4a7928921b5
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-06-01 23:59:53 +0530

    Time: 15 ms (73.74%), Space: 10.3 MB (49.90%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -39,9 +39,66 @@ public:
     }
     int tabulation(vector<vector<int>>&grid)
     {
-        return 0;
+        int rows=grid.size();
+        int cols=grid[0].size();
+        vector<vector<int>>dp(rows+1,vector<int>(cols+1));
+        /*
+         ==> fill up the base cases
+         ==> from the bases start building the solution to the sub problems
+         
+         
+         ==>Base Cases :
+            dp[0][...j]=matrix[0][...j]
+            
+         ==>Building Solution :   
+            we need to store the min of three  recurring solutions like below
+        int directly_below      = dfs(grid,r+1,c,dp);
+        int diagonally_left     = dfs(grid,r+1,c-1,dp); 
+        int diagonally_right    = dfs(grid,r+1,c+1,dp);
+        */
+        for(int i=0;i<cols;i++)
+        {
+            dp[0][i]=grid[0][i];
+        }
+        
+        for(auto i=1;i<rows;i++)
+        {
+            for(auto j=0;j<cols;j++)
+            {
+                int directly_below      = grid[i][j]+dp[i-1][j];
+                int diagonally_left     = grid[i][j]; 
+                int diagonally_right    = grid[i][j];
+                
+                //left
+                if(j-1>=0)
+                {
+                    diagonally_left +=dp[i-1][j-1];
+                }
+                else
+                {
+                    diagonally_left =INT_MAX;   
+                }
+                //right
+                if(j+1<rows)
+                {
+                    diagonally_right +=dp[i-1][j+1];
+                }
+                else
+                {
+                    diagonally_right =INT_MAX;
+                }
+                dp[i][j]=min(directly_below,min(diagonally_left,diagonally_right));
+            }
+        }
+        int ans=INT_MAX;
+        for(auto i=0;i<cols;i++)
+        {
+            ans=min(ans,dp[rows-1][i]);
+        }
+        return ans;
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        return memoization(matrix);
+        // return memoization(matrix);
+        return tabulation(matrix);
     }
 };
