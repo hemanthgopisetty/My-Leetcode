eb9889adfb751cb387e7c010ec3df042a9cee541
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-08-14 23:44:55 +0530

    Time: 238 ms (100.00%), Space: 116.6 MB (33.33%) - LeetHub

---
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+/*
+* Iterative  => reverse the list
+*               traverse the reversed list
+*               by carraying a carry
+*               and again reverse the new linked list 
+*
+
+* Recursive  => Single call (o to n) Non Tail Recursion
+*               Using NT recursion to eliminate reverse function 
+*               as we return carry in every NT recursion
+*/
+class Solution {
+public:
+    //retrun carry
+    int recurs(ListNode*cur)
+    {
+     
+        //recurs until no nodes are there
+        
+        if(!cur)return 0;
+        
+        //Non tail recursion
+        int carry =cur->val*2 + recurs(cur->next);
+        
+        //replace value
+        cur->val = carry%10;
+        
+        //return carry
+        
+        return carry/10;
+        
+    }
+    ListNode* doubleIt(ListNode* head) {
+        
+        int carry = recurs(head);
+        
+        if(carry) head = new ListNode(carry,head);
+        
+        return head;
+        
+    }
+};
