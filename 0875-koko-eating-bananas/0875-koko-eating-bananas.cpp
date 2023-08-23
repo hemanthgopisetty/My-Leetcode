74c115a60954c605cd7b9c94edc00bc227da0c02
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-08-23 16:19:39 +0530

    Time: 38 ms (77.90%), Space: 19 MB (44.68%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool can_koku_eat(vector<int>& piles,int speed,int h)
+    {
+        long long total_hours =0;
+        for(auto it:piles)
+        {
+            total_hours+=ceil(it/(double)speed);
+        }
+        return total_hours<=h;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        int l=1,r=*(max_element(begin(piles),end(piles)));
+        int ans =-1;
+        while(l<=r)
+        {
+            int mid = l+(r-l)/2;
+            
+            if(can_koku_eat(piles,mid,h))
+            {
+                ans=mid;
+                r=mid-1;
+            }
+            else
+            {
+                l=mid+1;
+            }
+        }
+        
+        return ans ;
+    }
+};
