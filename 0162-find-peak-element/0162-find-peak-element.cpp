3dcf538397855ee6b47f41802db3ea2b5c0f3690
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-08-24 13:01:34 +0530

    Time: 2 ms (78.59%), Space: 8.8 MB (95.25%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -11,10 +11,16 @@ public:
         {
             int mid = high+(low-high)/2;
             
+            //  prev < middle > next  => peak condition 
             if(nums[mid]>nums[mid-1] and nums[mid]>nums[mid+1]) return mid ;
+            //next > mid move to right space solution
             else if(nums[mid+1]>nums[mid]) low = mid+1;
+            //prev>mid move to the left space sloution 
             else if(nums[mid-1]>=nums[mid]) high = mid-1;
         }
+        
+        //check first two element and last two elements 
+        
         if(nums[0]>nums[1])return 0;
         else if(nums[n-1]>nums[n-2])return n-1;
         
