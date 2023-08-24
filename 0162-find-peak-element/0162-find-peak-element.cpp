0cddca23665332c3cac81d12d7f8495e8e21e7fb
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-08-24 14:11:54 +0530

    Time: 6 ms (33.40%), Space: 8.9 MB (14.15%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,29 +1,43 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n= nums.size();
-        if(n==1) return 0;
-        if(n==2) return nums[0]>nums[1]?0:1;
+        int n = nums.size();
         
-        int low=1,high=n-2; 
+        if(n==1) return  0;
+        if(n==2)  return nums[0]>nums[1] ? 0 : 1;
+        
+        //leave the first and last element 
+        int low = 1,high=n-2 ;
         
         while(low<=high)
         {
-            int mid = high+(low-high)/2;
+            //calculate the mid 
+            int mid = high + (low-high)/2;
             
-            //  prev < middle > next  => peak condition 
-            if(nums[mid]>nums[mid-1] and nums[mid]>nums[mid+1]) return mid ;
-            //next > mid move to right space solution
-            else if(nums[mid+1]>nums[mid]) low = mid+1;
-            //prev>mid move to the left space sloution 
-            else if(nums[mid-1]>=nums[mid]) high = mid-1;
+            //check wether mid is peak or not
+            if( (nums[mid]>nums[mid-1])  and (nums[mid]>nums[mid+1]) )
+            {
+                return mid ;
+            }
+            else if(nums[mid+1]>nums[mid])
+            {
+                low = mid+1;
+            }
+            else if(nums[mid-1]>=nums[mid])
+            {
+                high = mid-1;
+            }
         }
         
-        //check first two element and last two elements 
+        if(nums[0]>nums[1])
+        {
+            return 0;
+        }
+        else if(nums[n-1]>nums[n-2])
+        {
+            return n-1;
+        }
         
-        if(nums[0]>nums[1])return 0;
-        else if(nums[n-1]>nums[n-2])return n-1;
-        
-        return -1 ;
+        return -1;
     }
 };
