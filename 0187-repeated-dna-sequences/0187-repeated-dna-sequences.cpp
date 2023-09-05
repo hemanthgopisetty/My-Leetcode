938d942f120652be1cc934f30fd31ec497e4050e
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2023-09-05 12:59:20 +0530

    Time: 41 ms (83.93%), Space: 17.4 MB (90.44%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        //For every 10 character string  we need to store there result in the hashmap
+        //traverse the hashmap , mantain a ans variable if it's more than the ans then 
+        //modify that .
+        
+        
+        //buteforce 
+        //for every i traverse 10 letter character string from i
+        //mantain a answer varaible 
+        
+        
+        //storing the string in the hashmap would give us memory limit excced
+        hash<string> hfn;
+        unordered_map<size_t,int> hm ;
+        vector<string>ans ;
+        int n = s.size();
+        for(int i=0;i<n-9;i++)//traverse upto last but 9 digits
+        {
+            string temp = s.substr(i,10);
+            if(hm[hfn(temp)]++ == 1)
+            {
+                ans.push_back(temp);
+            }
+        }
+        return ans ;
+    }
+};
