134f3b4cb8d48c19e0906121aa9dac541085236b
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2024-03-25 11:50:37 +0530

    Time: 51 ms (48.98%), Space: 37.1 MB (51.91%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -3,6 +3,9 @@ public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> ans ;
         int n = nums.size();
+    /*
+        Visited or Not via marking O(n) and O(1) => TC nd SC
+        
         for(int i=0;i<n;i++)
         {
             //if visited mark it as visited
@@ -18,5 +21,34 @@ public:
             nums[abs(nums[i])-1] = -nums[abs(nums[i])-1] ;
         }
         return ans ;
+        
+    */
+    
+      //Grouping all duplicates together
+      //traversing each element
+      //xor the curr ele with prev ele
+      // if xor result is zero then we found out duplicate
+      //else its' not a duplicate
+    //if we found our diplicate let's move curr_idx to next idx
+    //and set xor element as next_idx to skip reudant checking of a duplicate
+       if(nums.empty())return ans ; 
+       sort(nums.begin(),nums.end()) ;
+       int s = nums[0];
+       for(int curr_idx = 1 ; curr_idx < n ; curr_idx++)
+       {
+           if(!(s^nums[curr_idx])) //if curr is dup
+           {
+               ans.push_back(nums[curr_idx]) ;
+               curr_idx+=1;
+               
+               if(curr_idx<n)s=nums[curr_idx];
+               else break ;
+           }
+           else 
+           {
+               s=nums[curr_idx];
+           }
+       }
+       return ans ;
     }
 };
