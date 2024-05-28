dd2086d0ce2dc8d363b74c49abbacc6723968535
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2024-05-28 09:05:38 +0530

    Time: 179 ms (44.79%), Space: 183.9 MB (59.71%) - LeetHub

--- 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.cpp
+++ 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
     vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries, int x) {
-       unordered_map<int,int> x_map;
+//        unordered_map<int,int> x_map;
         
-        int count=0;
-       for(int i=0;i<nums.size();i++)
-       {
-           if(nums[i]==x)
-           {
-               count++;
-               x_map[count]=i;// i -> index
-           }
-       }
+//         int count=0;
+//        for(int i=0;i<nums.size();i++)
+//        {
+//            if(nums[i]==x)
+//            {
+//                count++;
+//                x_map[count]=i;// i -> index
+//            }
+//        }
     
+//         for(int i=0;i<queries.size();i++){//q.logq
+//             if(x_map.find(queries[i])!=x_map.end())//if in map(logn)
+//             {
+//                 queries[i]=x_map[queries[i]];
+//             }
+//             else
+//             {
+//                  queries[i]=-1;
+//             }
+//         }
+//         return queries;
+        
+        vector<int>ans,xidx;
+        for(int i=0;i<nums.size();i++)if(nums[i]==x)xidx.push_back(i);
         for(int i=0;i<queries.size();i++){
-            if(x_map.find(queries[i])!=x_map.end())//if in map
-            {
-                queries[i]=x_map[queries[i]];
-            }
-            else
-            {
-                 queries[i]=-1;
-            }
+            ans.push_back(queries[i]-1<xidx.size()?xidx[queries[i]-1]:-1);
         }
-        return queries;
+        return ans ;
     }
 };
