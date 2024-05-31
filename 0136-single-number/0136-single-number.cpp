8ff10cc3d1b2ae07a073367f83f078e6b8d8728e
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2024-05-31 13:06:44 +0530

    Time: 9 ms (88.08%), Space: 19.2 MB (58.15%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
-    //every element appears twice except for one
-    //find the single one
-    
-    //we could use map here to find the frequencies
-    //and return an element that appers the exactly once
-    //this would take N time complexity and N space Complexity
-    
-    //we need to think like when we access an element
-    //we should get identified by no of times we had visited this
-    //In this way we will get Linear time and space complexity.
-    
-    //what to do then ??
-    //Bit manipulation :) To find the every element appears twice except for one 
-    //Pproperites A^A 0 A^B B
-    
     int singleNumber(vector<int>& nums) {
         int ans=0;
-        for(auto it:nums)ans^=it;
-        return ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            ans^=nums[i];
+        }
+        return ans ;
     }
 };
