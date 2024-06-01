2c2c9048efacf7eca68c55b26acefa6af90db73b
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2024-06-01 13:59:05 +0530

    Time: 5 ms (79.17%), Space: 12.3 MB (87.19%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        // In given array , there are elements that appears twice
+        // and two elements that appears one
+        // find two elements that appears once
+        
+        // O(n^2)TC , O(1)SC -> Bute Force -> Nested Loop
+        
+        // O(n)TC , O(n)SC -> Optimised -> Single Pass using HashMap
+        
+        // Given Follow up was this 
+        // O(n)TC , O(1)
+        
+        
+        // we need to something with this elements appears twice 
+        // two elements appears single
+int xor2no = 0;
+        for (int num : nums) {
+            xor2no ^= num;
+        }
+
+        // Cast xor2no to unsigned to avoid undefined behavior with -INT_MIN
+        unsigned int lowestBit = xor2no & -(unsigned int)xor2no;
+
+        vector<int> result(2, 0);
+        for (int num : nums) {
+            if ((lowestBit & num) == 0) {
+                result[0] ^= num;
+            } else {
+                result[1] ^= num;
+            }
+        }
+
+        return result;
+    }
+};
+    
