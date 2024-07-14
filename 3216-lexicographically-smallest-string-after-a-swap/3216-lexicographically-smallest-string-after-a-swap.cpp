fb9401840501a72b76a2594f8ec7a86e4f5530e3
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2024-07-14 12:32:05 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (83.33%) - LeetHub

---
+++ 3216-lexicographically-smallest-string-after-a-swap/3216-lexicographically-smallest-string-after-a-swap.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string getSmallestString(string s) {
+        for(int i=1;i<s.size();i++)
+        {
+            if((s[i-1]%2 == s[i]%2 ) and (s[i-1]>s[i]))
+            {
+                swap(s[i-1],s[i]);
+                break;
+            }
+        }
+        return s;
+    }
+};
