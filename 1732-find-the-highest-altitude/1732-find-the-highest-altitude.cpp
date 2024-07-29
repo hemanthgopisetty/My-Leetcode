456df91de22b417bf9c962c3ec230df76c1f5378
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2024-07-29 21:41:57 +0530

    Time: 0 ms (100.00%), Space: 10.4 MB (9.11%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int n=gain.size();
-        vector<int>a(n+1,0);
-        for(auto i=1;i<n+1;i++)
-        {
-            a[i]=a[i-1]+gain[i-1];
-        }
-        for(auto it:a)
-        {
-            cout<<it<<" ";
-        }
-        return *max_element(begin(a),end(a));
+         int n = gain.size();
+         vector<int>altitude(n+1,0);
+         int ans = altitude[0];
+         for(int i=1;i<n+1;i++)
+         {
+             altitude[i]=altitude[i-1]+gain[i-1];
+             cout<<altitude[i]<<endl;
+             ans = max(ans,altitude[i]);
+         }
+         
+        return ans ;
     }
 };
+//  altitude[0]=0
+//  altitude[1]=altitude[i-1]+gain[i-1] = 0+-5 = -5
+//  altitude[2]=altitude[i-1]+gain[i-1] = -5+1 = -4
+//  altitude[3]=altitude[i-1]+gain[i-1] = -4+5 = 1
+//  altitude[4]=altitude[i-1]+gain[i-1] = 1+0 = 1
+//  altitude[5]=altitude[i-1]+gain[i-1] = 1-7 = -6
