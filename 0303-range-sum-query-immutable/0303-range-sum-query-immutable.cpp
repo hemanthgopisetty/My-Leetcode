66be30b7a7f3e7d44c3139a34b1a47d06e873506
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2024-07-29 21:59:28 +0530

    Time: 21 ms (49.66%), Space: 22.1 MB (7.28%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -0,0 +1,27 @@
+class NumArray {
+public:
+    vector<int>preSum;
+    NumArray(vector<int>& nums) : preSum(nums) {
+      for(int i=1;i<nums.size();i++)
+      {
+          preSum[i]=preSum[i-1]+nums[i];
+      }
+    }
+    
+    int sumRange(int left, int right) {
+        if(left==0)return preSum[right];
+        return preSum[right]-preSum[left-1];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
+
+/*
+* creare prefixSum 
+* Intilaze them during constructor
+* Then sum Range will be called then we can return l to r sum in O(1)
+*/
