2ccb3df4be8a793a1a7433a34dd8cde9ddccea8d
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2024-07-30 19:27:32 +0530

    Time: 4 ms (79.31%), Space: 28 MB (83.76%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int ones = 0;
+        int twos = 0;
+        int n = nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>9)//two digit
+            {
+                twos+=nums[i];
+            }
+            else 
+            {
+                ones+=nums[i];
+            }
+        }
+        return ones == twos ? false : true ;
+    }
+};
