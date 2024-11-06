386faba998d44b1a37956caa2a706eddf78c64c8
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2024-11-06 16:57:19 +0530

    Time: 4 ms (34.53%), Space: 31.7 MB (33.78%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
@@ -1,46 +1,28 @@
 class Solution {
 public:
     bool canSortArray(vector<int>& nums) {
-        //Based on the given conditon 
-        //we can think , like same bits can be swapped in anyway
-        //so divide same bits elements as segments 
-        //track min and max of that segment
-        //if set bits were not same
-        //then if curr element is less than the currMin
-        //sorting not possible then return false
-        //set bits were not same and curr element is greater than the curMin
-        //update curMin , curMax,PrevMin , PrevMax
-        
-        int prevMax = INT_MIN;
-        int currMax = nums[0];
-        int currMin = nums[0];
-        int setBits = countBits(nums[0]);
-        
-        for(int i=1;i<nums.size();i++)
-        {
-            if(setBits == countBits(nums[i]))
-            {
-                currMin = min(currMin,nums[i]);
-                currMax = max(currMax,nums[i]);
-            }
-            else
-                //new segment
-            {
-                if(currMin<prevMax)return false;
-                    
-                prevMax = currMax;
-                setBits = countBits(nums[i]);
-                currMin = nums[i];
-                currMax = nums[i];
+        int n = nums.size();
+
+        // Avoid modifying the input directly
+        // Create a copy of the input array
+        vector<int> values = nums;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (values[j] <= values[j + 1]) {
+                    // No swap needed
+                    continue;
+                } else {
+                    if (__builtin_popcount(nums[j]) ==
+                        __builtin_popcount(nums[j + 1])) {
+                        // Swap the elements
+                        swap(values[j], values[j + 1]);
+                    } else {
+                        return false;
+                    }
+                }
             }
         }
-        
-        return currMin>prevMax;
-        
-    }
-    
-    private:
-    int countBits(int num) {
-        return std::bitset<32>(num).count();
+        return true;
     }
 };
