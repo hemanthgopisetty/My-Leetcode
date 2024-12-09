6d0eb5b169257b87f02dee4156e8f3daa825d245
Hemanth Gopisetty <84842917+hemanthgopisetty@users.noreply.github.com>
2024-12-09 17:04:01 +0530

    Time: 4 ms (87.50%), Space: 126.1 MB (83.60%) - LeetHub

--- 3152-special-array-ii/3152-special-array-ii.cpp
+++ 3152-special-array-ii/3152-special-array-ii.cpp
@@ -1,23 +1,41 @@
 class Solution {
-public:
-    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
-        int n=nums.size();
-        vector<int>ps(n,0);
-        for(int i=1;i<n;i++)
-        {
-            ps[i]=ps[i-1];
-            if(nums[i-1]%2 == nums[i]%2)
-            {
-                ps[i]++;
+private:
+    bool binarySearch(int start, int end, vector<int>& violatingIndices) {
+        int left = 0, right = violatingIndices.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (violatingIndices[mid] < start) {
+                left = mid + 1;
+            } else if (violatingIndices[mid] > end) {
+                right = mid - 1;
+            } else {
+                return true;
             }
         }
-        vector<bool>ans;
-        for(int i=0;i<queries.size();i++)
-        {
-            int from=queries[i][0];
-            int to=queries[i][1];
-            ans.push_back(ps[from]==ps[to]);
+        return false;
+    }
+
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        if (nums.empty()) return vector<bool>(queries.size(), true);
+        if (queries.empty()) return {};
+
+        int n = nums.size();
+        vector<int> violatingIndices;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] % 2 == nums[i - 1] % 2) {
+                violatingIndices.push_back(i);
+            }
         }
+
+        int q = queries.size();
+        vector<bool> ans(q);
+        for (int i = 0; i < q; i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+            ans[i] = !binarySearch(start + 1, end, violatingIndices);
+        }
+
         return ans;
     }
 };
